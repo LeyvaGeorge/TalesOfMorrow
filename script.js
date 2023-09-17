ea8efacdb2e65b714c33e8197534b9bb9d667d49
George Leyva <leyvageorge@hotmail.com>
2023-09-16 19:08:38 -0700

    changed nav to be more responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+// Navigation 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n=> n.
+addEventListener("click",() => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+// Main
+// Footer
+// -> Doesn't need any
